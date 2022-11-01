df0d7c20c5afe172e45532c8db4f851e123d986b
FranAGC <frcontreras.78@gmail.com>
2022-11-01 11:43:19 -0600

    Mejoras token admin

--- src/app/services/estadios.service.ts
+++ src/app/services/estadios.service.ts
@@ -6,47 +6,46 @@ import {HttpHeaders} from '@angular/common/http'
 import {Estadios} from '../models/Estadios'
 import { Observable } from 'rxjs';
 
+function adminHeader() {
+  const token = localStorage.getItem('token')
+  return new HttpHeaders({
+    'Authorization': `${token}`
+  });
+}
+
 const headers = new HttpHeaders({
   'Content-Type': 'application/json',
   'Authorization':
   `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkRhaW5hMiIsImlhdCI6MTY2NjQ1MjU3NiwiZXhwIjoxNjY5MDQ0NTc2fQ.FXouHuO4lsGC5YjrsgLxiktkZBdPLLwEvL7ROZmqLmg`
-
-});
-
-const token = localStorage.getItem('token')
-
-const header = new HttpHeaders({
-  'Content-Type': 'application/json',
-  'Authorization': `${token}`
-
 });
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EstadiosService {
   API_URI = 'http://localhost:3000/api/v1'
 
   constructor(private http: HttpClient) { }
 
   getEstadios(){
-    return this.http.get(`${this.API_URI}/estadios`,{headers: headers});
+    return this.http.get(`${this.API_URI}/estadios`,{headers:headers});
   }
 
   getEstadio(id:string){
-    return this.http.get(`${this.API_URI}/estadios/${id}`, {headers: headers});
+    return this.http.get(`${this.API_URI}/estadios/${id}`, {headers:headers});
   }
 
   saveEstadio(estadio: Object){
-    return this.http.post(`${this.API_URI}/estadios`, estadio,{headers:header});
+    return this.http.post(`${this.API_URI}/estadios`, estadio,{headers:adminHeader()});
   }
 
   deleteEstadio(id:string){
-    return this.http.delete(`${this.API_URI}/estadios/${id}`,{headers: header});
+    return this.http.delete(`${this.API_URI}/estadios/${id}`,{headers:adminHeader()});
   }
 
   updateEstadio(id:string, updatedEstadio:Object){
-    return this.http.put(`${this.API_URI}/estadios/${id}`, updatedEstadio,{headers: header});
+    return this.http.put(`${this.API_URI}/estadios/${id}`, updatedEstadio,{headers:adminHeader()});
   }
 }
