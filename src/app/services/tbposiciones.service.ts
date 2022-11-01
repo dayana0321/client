8982a40f1e7f1fc9e62b239a6b9a1aa4c65d3409
FranAGC <frcontreras.78@gmail.com>
2022-11-01 11:48:22 -0600

    Mejoras token admins
    ? :(

--- src/app/services/tbposiciones.service.ts
+++ src/app/services/tbposiciones.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
+
+
 const headers = new HttpHeaders({
   'Content-Type': 'application/json',
   'Authorization':
   `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkRhaW5hMiIsImlhdCI6MTY2NjQ1MjU3NiwiZXhwIjoxNjY5MDQ0NTc2fQ.FXouHuO4lsGC5YjrsgLxiktkZBdPLLwEvL7ROZmqLmg`
-
 });
-const token = localStorage.getItem('token')
 
-const header = new HttpHeaders({
-  'Content-Type': 'application/json',
-  'Authorization': `${token}`
 
-});
+function adminHeader() {
+  const token = localStorage.getItem('token')
+  return new HttpHeaders({
+    'Authorization': `${token}`
+  });
+}
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TbposicionesService {
   API_URI = 'http://localhost:3000/api/v1'
 
@@ -33,14 +36,14 @@ export class TbposicionesService {
   }
 
   SaveTbposiciones(resultado: Object){
-    return this.http.post(`${this.API_URI}/tbposiciones`, resultado, {headers:header});
+    return this.http.post(`${this.API_URI}/tbposiciones`, resultado, {headers:adminHeader()});
   }
 
   deleteTbposiciones(id:string){
-    return this.http.delete(`${this.API_URI}/tbposiciones/${id}`, {headers: header});
+    return this.http.delete(`${this.API_URI}/tbposiciones/${id}`, {headers:adminHeader()});
   }
 
   updateTbposiciones(id:string, updatedPais:Object){
-    return this.http.put(`${this.API_URI}/tbposiciones/${id}`, updatedPais, {headers:header} );
+    return this.http.put(`${this.API_URI}/tbposiciones/${id}`, updatedPais, {headers:adminHeader()} );
   }
 }
