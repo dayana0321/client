8a822d575b3ff8feb12d1a8f262e42e67f513c99
FranAGC <frcontreras.78@gmail.com>
2022-11-01 11:45:32 -0600

    Mejoras token admins

--- src/app/services/resultados.service.ts
+++ src/app/services/resultados.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
+
+
+function adminHeader() {
+  const token = localStorage.getItem('token')
+  return new HttpHeaders({
+    'Authorization': `${token}`
+  });
+}
+
 const headers = new HttpHeaders({
   'Content-Type': 'application/json',
   'Authorization':
   `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkRhaW5hMiIsImlhdCI6MTY2NjQ1MjU3NiwiZXhwIjoxNjY5MDQ0NTc2fQ.FXouHuO4lsGC5YjrsgLxiktkZBdPLLwEvL7ROZmqLmg`
-
-});
-const token = localStorage.getItem('token')
-
-const header = new HttpHeaders({
-  'Content-Type': 'application/json',
-  'Authorization': `${token}`
 
 });
 
@@ -20,8 +22,8 @@ const header = new HttpHeaders({
 })
 export class ResultadosService {
   API_URI = 'http://localhost:3000/api/v1'
-  constructor(private http:HttpClient) { } 
-  
+  constructor(private http:HttpClient) { }
+
   getResultados(){
     return this.http.get(`${this.API_URI}/resultados`, {headers: headers});
   }
@@ -31,16 +33,15 @@ export class ResultadosService {
   }
 
   saveResultado(resultado: Object){
-    return this.http.post(`${this.API_URI}/resultados`, resultado, {headers:header});
+    return this.http.post(`${this.API_URI}/resultados`, resultado, {headers:adminHeader()});
   }
 
   deleteResultado(id:string){
-    return this.http.delete(`${this.API_URI}/resultados/${id}`, {headers: header});
+    return this.http.delete(`${this.API_URI}/resultados/${id}`, {headers:adminHeader()});
   }
 
   updateResultado(id:string, updatedPais:Object){
-    return this.http.patch(`${this.API_URI}/resultados/${id}`, updatedPais, {headers:header} );
+    return this.http.patch(`${this.API_URI}/resultados/${id}`, updatedPais, {headers:adminHeader()} );
   }
 }
-
 
